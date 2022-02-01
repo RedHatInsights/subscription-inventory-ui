a7a960ecc00845a3f7fbf61cc4301b042bf2a204
Zane Woodfin <pwoodfin@redhat.com>
2022-02-01 16:54:46 -0500

    Check read permission

--- src/pages/SubscriptionInventoryPage/SubscriptionInventoryPage.tsx
+++ src/pages/SubscriptionInventoryPage/SubscriptionInventoryPage.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { withRouter } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
 import Main from '@redhat-cloud-services/frontend-components/Main';
 import PageHeader from '@redhat-cloud-services/frontend-components/PageHeader';
 import { PageSection, Title } from '@patternfly/react-core';
@@ -12,19 +12,27 @@ const SubscriptionInventoryPage: FunctionComponent = () => {
   const queryClient = useQueryClient();
   const user: User = queryClient.getQueryData('user');
 
-  return (
-    <>
-      <PageHeader>
-        <PageHeaderTitle title="Subscription Inventory" />
-      </PageHeader>
-      <Main>
-        <PageSection variant="light">
-          <Title headingLevel="h2">All subscriptions for account {user.accountNumber}</Title>
-          <ProductsTable />
-        </PageSection>
-      </Main>
-    </>
-  );
+  const Page: FunctionComponent = () => {
+    return (
+      <>
+        <PageHeader>
+          <PageHeaderTitle title="Subscription Inventory" />
+        </PageHeader>
+        <Main>
+          <PageSection variant="light">
+            <Title headingLevel="h2">All subscriptions for account {user.accountNumber}</Title>
+            <ProductsTable />
+          </PageSection>
+        </Main>
+      </>
+    );
+  };
+
+  if (user.canReadProducts) {
+    return <Page />;
+  } else {
+    return <Redirect to="/no-permissions" />;
+  }
 };
 
 export default withRouter(SubscriptionInventoryPage);
