2aec28fc86ab452741a3162ffb42941d38b04fc8
Zane Woodfin <pwoodfin@redhat.com>
2022-03-24 08:44:45 -0400

    TEAMNADO-2875 add text search (#5)
    * Add text search
    
    * Update search placeholder text
    
    * Fix per-character search focus loss
    
    * Fix test coverage
    
    * Allow search by product line
    
    * Add default sort
    
    * Remove unused import

--- src/components/ProductsTable/__tests__/ProductsTable.test.tsx
+++ src/components/ProductsTable/__tests__/ProductsTable.test.tsx
@@ -1,94 +1,60 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { render, screen, fireEvent } from '@testing-library/react';
-import ProductsTable from '../ProductsTable';
-import useProducts from '../../../hooks/useProducts';
+import ProductsTable, { ProductsTableProps } from '../ProductsTable';
 import { get, def } from 'bdd-lazy-var';
 import factories from '../../../utilities/factories';
 
-jest.mock('../../../hooks/useProducts');
-jest.mock('uuid', () => {
-  return { v4: jest.fn(() => '00000000-0000-0000-0000-000000000000') };
-});
-
 const queryClient = new QueryClient();
 
-const Table = () => (
+const Table: FunctionComponent<ProductsTableProps> = ({ data, isFetching }) => (
   <QueryClientProvider client={queryClient}>
-    <ProductsTable />
+    <ProductsTable data={data} isFetching={isFetching} />
   </QueryClientProvider>
 );
 
 describe('ProductsTable', () => {
   def('loading', () => false);
   def('error', () => false);
+  def('fetching', () => false);
   def('data', () => [factories.product.build({ name: 'A', productLine: 'letters', quantity: 3 })]);
 
-  beforeEach(() => {
-    (useProducts as jest.Mock).mockReturnValue({
-      isLoading: get('loading'),
-      error: get('error'),
-      data: get('data')
-    });
-  });
-
   it('renders correctly', () => {
-    const { container } = render(<Table />);
+    const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
     expect(container).toMatchSnapshot();
-  });
-
-  describe('when loading', () => {
-    def('loading', () => true);
-
-    it('renders the loading state', () => {
-      const { container } = render(<Table />);
-
-      expect(container).toMatchSnapshot();
-    });
-  });
-
-  describe('when there is an error', () => {
-    def('error', () => true);
-
-    it('renders the error state', () => {
-      const { container } = render(<Table />);
-
-      expect(container).toMatchSnapshot();
-    });
   });
 
   describe('when row column headings are clicked', () => {
     def('data', () => [
       factories.product.build({ name: 'Z', quantity: 1, productLine: 'letters' }),
-      factories.product.build({ name: 'A', quantity: 3, productLine: 'letters' }),
-      factories.product.build({ name: 'B', quantity: 2, productLine: 'letters' })
+      factories.product.build({ name: undefined, quantity: undefined, productLine: null }),
+      factories.product.build({ name: 'A', quantity: 3, productLine: 'vowels' }),
+      factories.product.build({ name: null, quantity: 2, productLine: 'consonants' })
     ]);
 
-    it('can sort by name', () => {
-      const { container } = render(<Table />);
+    it('sorts by name by default', () => {
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
-      fireEvent.click(screen.getByText('Name'));
       expect(container).toMatchSnapshot();
     });
 
     it('can sort by name, reversed', () => {
-      const { container } = render(<Table />);
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
-      fireEvent.click(screen.getByText('Name'));
       fireEvent.click(screen.getByText('Name'));
       expect(container).toMatchSnapshot();
     });
 
     it('can sort by quantity', () => {
-      const { container } = render(<Table />);
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
       fireEvent.click(screen.getByText('Quantity'));
       expect(container).toMatchSnapshot();
     });
 
     it('can sort by quantity, reversed', () => {
-      const { container } = render(<Table />);
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
       fireEvent.click(screen.getByText('Quantity'));
       fireEvent.click(screen.getByText('Quantity'));
@@ -103,7 +69,7 @@ describe('ProductsTable', () => {
     ]);
 
     it('can change page', () => {
-      const { container } = render(<Table />);
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
       const nextPage = screen.getAllByLabelText('Go to next page')[0];
       fireEvent.click(nextPage);
@@ -111,12 +77,47 @@ describe('ProductsTable', () => {
     });
 
     it('can change per page', () => {
-      const { container } = render(<Table />);
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
 
       const perPageArrow = screen.getAllByLabelText('Items per page')[0];
       fireEvent.click(perPageArrow);
       const perPageAmount = screen.getByText('20 per page');
       fireEvent.click(perPageAmount);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('when the search is used', () => {
+    def('data', () => [
+      factories.product.build({ name: 'Z', quantity: 2, productLine: 'consonants' }),
+      factories.product.build({ name: 'A', quantity: 1, productLine: 'vowels' }),
+      factories.product.build({ name: 'P', quantity: 1, productLine: 'consonants' }),
+      factories.product.build({ name: undefined, quantity: 0, productLine: undefined })
+    ]);
+
+    it('refines the results by name', () => {
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
+
+      const input = screen.getByPlaceholderText('Filter by Name');
+      fireEvent.change(input, { target: { value: 'Z' } });
+      expect(container).toMatchSnapshot();
+    });
+
+    it('refines the results by product line', () => {
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
+
+      const input = screen.getByPlaceholderText('Filter by Name');
+      fireEvent.change(input, { target: { value: 'vowels' } });
+      expect(container).toMatchSnapshot();
+    });
+
+    it('can be cleared', () => {
+      const { container } = render(<Table data={get('data')} isFetching={get('fetching')} />);
+
+      const input = screen.getByPlaceholderText('Filter by Name');
+      fireEvent.change(input, { target: { value: 'Z' } });
+      const clear = screen.getByLabelText('Reset');
+      fireEvent.click(clear);
       expect(container).toMatchSnapshot();
     });
   });
--- src/pages/SubscriptionInventoryPage/SubscriptionInventoryPage.tsx
+++ src/pages/SubscriptionInventoryPage/SubscriptionInventoryPage.tsx
@@ -2,15 +2,19 @@ import React, { FunctionComponent } from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import Main from '@redhat-cloud-services/frontend-components/Main';
 import PageHeader from '@redhat-cloud-services/frontend-components/PageHeader';
+import Unavailable from '@redhat-cloud-services/frontend-components/Unavailable';
 import { PageSection, Title } from '@patternfly/react-core';
 import { PageHeaderTitle } from '@redhat-cloud-services/frontend-components/PageHeader';
 import { useQueryClient } from 'react-query';
 import { User } from '../../hooks/useUser';
 import ProductsTable from '../../components/ProductsTable';
+import { Processing } from '../../components/emptyState';
+import useProducts from '../../hooks/useProducts';
 
 const SubscriptionInventoryPage: FunctionComponent = () => {
   const queryClient = useQueryClient();
   const user: User = queryClient.getQueryData('user');
+  const { isFetching, isLoading, error, data } = useProducts();
 
   const Page: FunctionComponent = () => {
     return (
@@ -21,7 +25,13 @@ const SubscriptionInventoryPage: FunctionComponent = () => {
         <Main>
           <PageSection variant="light">
             <Title headingLevel="h2">All subscriptions for account {user.accountNumber}</Title>
-            <ProductsTable />
+            <>
+              {isLoading && !error && <Processing />}
+
+              {!isLoading && !error && <ProductsTable data={data} isFetching={isFetching} />}
+
+              {error && <Unavailable />}
+            </>
           </PageSection>
         </Main>
       </>
