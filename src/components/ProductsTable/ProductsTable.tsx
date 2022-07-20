44de1316add8282d093dd8d811d809301f386e4c
Reed Cowan <recowan@recowan.remote.csb>
2022-07-20 14:35:00 -0400

    Fixed linting issue

--- src/components/ProductsTable/ProductsTable.tsx
+++ src/components/ProductsTable/ProductsTable.tsx
@@ -254,9 +254,7 @@ const ProductsTable: FunctionComponent<ProductsTableProps> = ({
           ))}
         </Tbody>
       </TableComposable>
-      {paginatedProducts.length == 0 && (
-        <NoSearchResults clearFilters={clearSearch} />
-      )}
+      {paginatedProducts.length == 0 && <NoSearchResults clearFilters={clearSearch} />}
       {pagination(PaginationVariant.bottom)}
     </>
   );
