32b0992d8f33ed56e887d201493c41cc2c7f7458
Reed Cowan <reedcowan30@gmail.com>
2022-09-20 15:06:26 -0400

    Formatted doc

--- src/components/GettingStartedCard/__tests__/GettingStartedCard.test.tsx
+++ src/components/GettingStartedCard/__tests__/GettingStartedCard.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import GettingStartedCard from '../GettingStartedCard';
 
 describe('GettingStartedCard', () => {
-  // This is an example of an unncessary test. If we write a test where 
+  // This is an example of an unncessary test. If we write a test where
   // we are checking the contents of the toggle button being selected and clicked,
   // then we don't need a test that checks if the button is there in the first place.
 
@@ -17,6 +17,8 @@ describe('GettingStartedCard', () => {
     const { getByTestId } = render(<GettingStartedCard />);
 
     fireEvent.click(getByTestId('resources-toggle-button'));
-    expect(screen.getByText('Activate a subscription purchased from a third party.')).toBeInTheDocument();
+    expect(
+      screen.getByText('Activate a subscription purchased from a third party.')
+    ).toBeInTheDocument();
   });
 });
