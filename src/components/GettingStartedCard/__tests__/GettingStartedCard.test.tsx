0fefa7a38fbea7426d40b999804a89df0842e7aa
Reed Cowan <reedcowan30@gmail.com>
2022-09-22 14:20:30 -0400

    Removed comments

--- src/components/GettingStartedCard/__tests__/GettingStartedCard.test.tsx
+++ src/components/GettingStartedCard/__tests__/GettingStartedCard.test.tsx
@@ -3,16 +3,6 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import GettingStartedCard from '../GettingStartedCard';
 
 describe('GettingStartedCard', () => {
-  // This is an example of an unncessary test. If we write a test where
-  // we are checking the contents of the toggle button being selected and clicked,
-  // then we don't need a test that checks if the button is there in the first place.
-
-  // it('renders a button', () => {
-  //   render(<GettingStartedCard />);
-
-  //   expect(document.body).toMatchSnapshot();
-  // });
-
   it('renders the card when the toggle is clicked', async () => {
     const { getByTestId } = render(<GettingStartedCard />);
 
