db59913f3cd3939f54ba17a372ed3145be2f5ffe
Luke Green <lgreen@redhat.com>
2023-02-07 15:14:01 -0600

    Change curl method

--- pr_check.sh
+++ pr_check.sh
@@ -27,7 +27,7 @@ BUILD_RESULTS=$?
 # deploy to ephemeral
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+source <(curl -sSL $CICD_URL/bootstrap.sh)
 
 export DEPLOY_FRONTENDS="true"
 export APP_NAME="rhsm-api-proxy"
