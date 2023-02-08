b845f23c29b5f4c795a60654e1f7fc3ad4bbdabe
Luke Green <lgreen@redhat.com>
2023-02-08 13:05:01 -0600

    check pip in bootstrap

--- pr_check.sh
+++ pr_check.sh
@@ -27,7 +27,9 @@ BUILD_RESULTS=$?
 # deploy to ephemeral
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh 
+sed 's/pip install --upgrade pip '\''setuptools<58'\'' wheel/cat $(which pip)\npip install --upgrade pip '\''setuptools<58'\'' wheel/' .cicd_bootstrap.sh
+source .cicd_bootstrap.sh
 
 export DEPLOY_FRONTENDS="true"
 export APP_NAME="rhsm-api-proxy"
