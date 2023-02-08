bb9ae9ea0a841eca6700237f895696c4ee44cb87
Luke Green <lgreen@redhat.com>
2023-02-08 08:26:41 -0600

    Add artifact to debug run

--- pr_check.sh
+++ pr_check.sh
@@ -27,7 +27,8 @@ BUILD_RESULTS=$?
 # deploy to ephemeral
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-source <(curl -sSL $CICD_URL/bootstrap.sh)
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+cp $CICD_URL/bootstrap.sh $WORKSPACE/artifacts/bootstrap.sh
 
 export DEPLOY_FRONTENDS="true"
 export APP_NAME="rhsm-api-proxy"
