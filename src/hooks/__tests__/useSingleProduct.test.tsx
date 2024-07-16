c3b42f6dd9ba87209cddc001af15c9313fca1bbc
blower <blower@redhat.com>
2024-07-16 08:52:55 -0400

    Added testing to low coverage files, to increase coverage threshold.

--- src/hooks/__tests__/useSingleProduct.test.tsx
+++ src/hooks/__tests__/useSingleProduct.test.tsx
@@ -1,41 +1,54 @@
 import { renderHook, waitFor } from '@testing-library/react';
 import fetch, { enableFetchMocks } from 'jest-fetch-mock';
-import { createQueryWrapper } from '../../utilities/testHelpers';
 import useSingleProduct from '../useSingleProduct';
+import { HttpError } from '../../utilities/errors';
+import { createQueryWrapper } from '../../utilities/testHelpers';
 
 enableFetchMocks();
+jest.mock('../../utilities/platformServices', () => ({
+  useToken: jest.fn()
+}));
 
 describe('useSingleProduct', () => {
-  beforeEach(() => {
-    Object.defineProperty(window, 'insights', {
-      value: {
-        chrome: {
-          auth: {
-            getToken: jest.fn()
-          }
-        }
-      }
-    });
-  });
+  const sku = 'TESTSKU';
+  const productData = {
+    name: 'TEST Name',
+    productLine: 'TEST Line',
+    quantity: 3,
+    sku: 'TESTSKU',
+    serviceLevel: 'TEST serviceLevel',
+    serviceType: 'TEST serviceType',
+    capacity: { name: 'test', quantity: 2 }
+  };
+
   it('returns a product from the API', async () => {
-    const productData = {
-      name: 'TEST Name',
-      productLine: 'TEST Line',
-      quantity: 3,
-      sku: 'TESTSKU',
-      serviceLevel: 'TEST serviceLevel',
-      serviceType: 'TEST serviceType',
-      capacity: { name: 'test', quantity: 2 }
-    };
-
     fetch.mockResponseOnce(JSON.stringify({ body: { ...productData } }));
-
-    const { result } = renderHook(() => useSingleProduct('TESTSKU'), {
+    const { result } = renderHook(() => useSingleProduct(sku), {
       wrapper: createQueryWrapper()
     });
-
     await waitFor(() => {
       expect(result.current.data).toEqual(productData);
     });
+  });
+  it('handles HTTP error', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ message: 'Not Found' }), { status: 404 });
+    const { result } = renderHook(() => useSingleProduct(sku), {
+      wrapper: createQueryWrapper()
+    });
+    await waitFor(() => {
+      expect(result.current.error).toBeInstanceOf(HttpError);
+      expect((result.current.error as HttpError).status).toBe(404);
+    });
+  });
+  it('does not retry on 404 error', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ message: 'Not Found' }), { status: 404 });
+    const { result } = renderHook(() => useSingleProduct(sku), {
+      wrapper: createQueryWrapper()
+    });
+    await waitFor(() => {
+      expect(result.current.error).toBeInstanceOf(HttpError);
+      expect((result.current.error as HttpError).status).toBe(404);
+    });
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 });
