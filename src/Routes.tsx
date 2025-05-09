669f3c598fdea25e87188a72d555a45780ff3769
Luke Green <lgreen@redhat.com>
2025-05-09 13:02:17 -0500

    add widget test page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,25 +2,36 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import { Processing } from './components/emptyState';
 import Authentication from './components/Authentication';
+import useFeatureFlag from './hooks/useFeatureFlag';
+import { SubsWidget } from './components/Widgets/SubscriptionsWidget';
 
 const SubscriptionInventoryPage = lazy(() => import('./pages/SubscriptionInventoryPage'));
 const OopsPage = lazy(() => import('./pages/OopsPage'));
 const NoPermissionsPage = lazy(() => import('./pages/NoPermissionsPage'));
 const DetailsPage = lazy(() => import('./pages/DetailsPage'));
 
-export const InventoryRoutes: React.FC = () => (
-  <div className="inventory">
-    <Suspense fallback={<Processing />}>
-      <Authentication>
-        <Routes>
-          <Route path="/" element={<SubscriptionInventoryPage />} />
-          <Route path="/oops" element={<OopsPage />} />
-          <Route path="/no-permissions" element={<NoPermissionsPage />} />
-          <Route path="/:SKU" element={<DetailsPage />} />
-          {/* Finally, catch all unmatched routes */}
-          <Route path="*" element={<Navigate to="/oops" replace />} />
-        </Routes>
-      </Authentication>
-    </Suspense>
-  </div>
-);
+export const InventoryRoutes: React.FC = () => {
+  const shouldShowWidgetTest = useFeatureFlag('subscription-inventory.show-test-widget');
+
+  return (
+    <div className="inventory">
+      <Suspense fallback={<Processing />}>
+        <Authentication>
+          <Routes>
+            <Route path="/" element={<SubscriptionInventoryPage />} />
+            <Route path="/oops" element={<OopsPage />} />
+            <Route path="/no-permissions" element={<NoPermissionsPage />} />
+            <Route path="/:SKU" element={<DetailsPage />} />
+
+            {(shouldShowWidgetTest || false) && (
+              <Route path="/widget-test" element={<SubsWidget />} />
+            )}
+
+            {/* Finally, catch all unmatched routes */}
+            <Route path="*" element={<Navigate to="/oops" replace />} />
+          </Routes>
+        </Authentication>
+      </Suspense>
+    </div>
+  );
+};
